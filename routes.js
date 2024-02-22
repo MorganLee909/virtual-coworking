282a5917e3ca2c6012a4245b8051ea6e1a0a6a10
Lee Morgan <me@leemorgan.io>
2024-02-22 10:20:31 -0500

    Add plain sign up page.

--- routes.js
+++ routes.js
@@ -2,4 +2,5 @@ module.exports = (app)=>{
     const views = `${__dirname}/views`;
 
     app.get("/", (req, res)=>res.sendFile(`${views}/landing.html`));
+    app.get("/signup", (req, res)=>{res.sendFile(`${views}/signup.html`)});
 }
