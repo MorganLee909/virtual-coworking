9fcdda30e5f9d63bdc1b45fec5ffed025e4434ec
Lee Morgan <me@leemorgan.io>
2024-02-22 12:49:25 -0500

    Change back and front ends to use JWT and JSON rather than standard forms.

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const User = require("../models/user.js");
 
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
     /*
@@ -15,15 +16,35 @@ module.exports = {
      * redirect = /dashboard
     */
     create: (req, res)=>{
-        if(!req.body.email) return res.redirect("/user/signup");
+        if(!req.body.email){
+            return res.json({
+                error: true,
+                message: "Invalid email address"
+            });
+        }
         let email = req.body.email.toLowerCase();
 
-        if(req.body.password.length < 10) return res.redirect("/user/signup");
-        if(req.body.password !== req.body.confirmPassword) return res.redirect("/user/signup");
+        if(req.body.password.length < 10){
+            return res.json({
+                error: true,
+                message: "Password must contain at least 10 characters"
+            });
+        }
+        if(req.body.password !== req.body.confirmPassword){
+            return res.json({
+                error: true,
+                message: "Passwords do not match"
+            });
+        }
 
         User.findOne({email: email})
             .then((user)=>{
-                if(user) return res.redirect("/user/signup");
+                if(user){
+                    return res.json({
+                        error: true,
+                        message: "User with this email already exists"
+                    });
+                }
 
                 let salt = bcrypt.genSaltSync(10);
                 let hash = bcrypt.hashSync(req.body.password, salt);
@@ -36,10 +57,16 @@ module.exports = {
                 });
 
                 return newUser.save();
-
             })
             .then((user)=>{
-                res.redirect("/dashboard");
+                let token = jwt.sign({
+                    _id: user._id,
+                    email: user.email,
+                    firstName: user.firstName,
+                    lastName: user.lastName
+                }, process.env.JWT_SECRET);
+
+                res.json(token);
             })
             .catch((err)=>{
                 console.error(err);
