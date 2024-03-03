f4b5c8f06fb9cf667921bf87de3ef1399fa1e381
Lee Morgan <me@leemorgan.io>
2024-03-03 12:28:42 -0500

    Add click and drag functionality for meetings. Add fullscreen functionality for meetings. Add adjustable screen size for meetings, although it is broken by the click and drag functionality.

--- views/coworking/js/dashboard.js
+++ views/coworking/js/dashboard.js
@@ -53,11 +53,62 @@ for(let i = 0; i < tables.length; i++){
 
 const socket = new WebSocket('ws://localhost:8000');
 socket.addEventListener("open", ()=>{
-    console.log("open");
     socket.send("Hello from client 1");
-    console.log("message sent");
 });
 
 socket.addEventListener("message", (event)=>{
     console.log(event.data);
 });
+
+const dragElement = (elem)=>{
+    const dragMouseDown = (e)=>{
+        e = e || window.event;
+        e.preventDefault();
+
+        positions[2] = e.clientX;
+        positions[3] = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    const elementDrag = (e)=>{
+        e = e || window.event;
+        e.preventDefault();
+
+        positions[0] = positions[2] - e.clientX;
+        positions[1] = positions[3] - e.clientY;
+        positions[2] = e.clientX;
+        positions[3] = e.clientY;
+
+        elem.style.top = (elem.offsetTop - positions[1]) + "px";
+        elem.style.left = (elem.offsetLeft - positions[0]) + "px";
+    }
+
+    const closeDragElement = ()=>{
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+
+    let positions = [];
+    elem.onmousedown = dragMouseDown;
+}
+
+const fullScreen = (event)=>{
+    let fs = event.target.getAttribute("data-fs");
+    if(fs === "false"){
+        event.target.style.height = "100%";
+        event.target.style.width = "100%";
+        event.target.style.top = "0";
+        event.target.style.left = "0";
+        event.target.setAttribute("data-fs", "true");
+    }else{
+        event.target.style.height = "50%";
+        event.target.style.width = "50%";
+        event.target.style.top = "25%";
+        event.target.style.left = "25%";
+        event.target.setAttribute("data-fs", "false");
+    }
+}
+
+document.getElementById("meeting").addEventListener("dblclick", fullScreen);
+dragElement(document.getElementById("meeting"));
