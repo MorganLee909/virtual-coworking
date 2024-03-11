3682a522cfd69c747204c36878f45bab0357bf9a
Lee Morgan <me@leemorgan.io>
2024-03-11 12:53:24 -0400

    Remove leftover stuff from merge conflict.

--- app.js
+++ app.js
@@ -23,13 +23,8 @@ let esbuildOptions = {
     keepNames: true,
     outdir: `${__dirname}/views/build/`,
     define: {
-<<<<<<< Updated upstream
         "process.env.SITE": JSON.stringify(process.env.SITE),
         "process.env.WS": JSON.stringify(process.env.NODE_ENV === "production" ? "wss" : "ws")
-=======
-        // "process.env.SITE": process.env.NODE_ENV === "production" ? "cosphere.work" : JSON.stringify("localhost:8000")
-        "process.env.SITE": JSON.stringify(process.env.SITE)
->>>>>>> Stashed changes
     }
 };
 
