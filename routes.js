0e60c8780773e0e81f2c349204768a546f68d09d
Lee Morgan <me@leemorgan.io>
2024-03-19 12:13:21 -0400

    Create 404 page.

--- routes.js
+++ routes.js
@@ -42,4 +42,6 @@ module.exports = (app)=>{
     //IMAGES
     app.get("/image/:image", (req, res)=>{res.sendFile(`${views}/image/${req.params.image}`)});
     app.get("/image/profile/:image", (req, res)=>{res.sendFile(`${appRoot}/profilePhoto/${req.params.image}`)});
+
+    app.get("*", (req, res)=>{res.sendFile(`${views}/404.html`)});
 }
