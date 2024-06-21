c8392695b9b4db075fe0a7de94cfdd1edab5e0cc
Lee Morgan <lee@leemorgan.dev>
2024-06-21 23:12:51 +0300

    Remove routes from the original routes file.

--- routes.js
+++ routes.js
@@ -1,17 +1,9 @@
-const user = require("./controllers/user.js");
 const stripe = require("./controllers/stripe.js");
-const newsletter = require("./controllers/newsletter.js");
-const location = require("./controllers/location.js")
 const office = require("./controllers/office.js");
 const {auth}= require("./auth.js");
 
 module.exports = (app)=>{
     const views = `${__dirname}/views`;
-
-    //LOCATION
-    //app.post("/location/table/join", auth, location.joinTable);
-    //app.get("/location/:locationId", auth, location.getOne);
-    //app.get("/location", auth, location.getAll);
 
     //OFFICE
     app.get("/office/location/:locationId", auth, office.getOffices);
